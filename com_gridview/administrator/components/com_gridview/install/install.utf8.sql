544e67e86dec7bf85922ae3063cecf9f12d4a752
Jonathan Bennett <jbennett@ccistudios.com>
2011-09-19 07:48:38 -0400

    Initial DB schema

---
+++ com_gridview/administrator/components/com_gridview/install/install.utf8.sql
@@ -0,0 +1,29 @@
+CREATE TABLE IF NOT EXISTS `#__gridview_gridviews` (
+	`gridview_gridview_id` SERIAL,
+	
+	`title` varchar (250) NOT NULL
+) DEFAULT;
+
+CREATE TABLE IF NOT EXISTS `#__gridview_pages` (
+	`gridview_page_id` SERIAL,
+	
+	`title` varchar (250) NOT NULL
+) DEFAULT;
+
+CREATE TABLE IF NOT EXISTS `#__gridview_tiles` (
+	`gridview_tile_id` SERIAL,
+	
+	`title` varchar (250) NOT NULL
+) DEFAULT;
+
+CREATE TABLE IF NOT EXISTS `#__gridview_images` (
+	`gridview_image_id` SERIAL,
+	
+	`title` varchar (250) NOT NULL
+) DEFAULT;
+
+
+
+# url.com/gridview/GRIDVIEW/PAGE/TILE/IMAGE
+# url.com/gridview/1/5/6
+# url.com/ccistudios/website-design/inkspot/2
