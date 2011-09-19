2572f7b817073be18350b708d571095cd710e750
Jonathan Bennett <jbennett@ccistudios.com>
2011-09-19 08:01:27 -0400

    Created grid view creation page

--- com_gridview/administrator/components/com_gridview/views/gridview/tmpl/form.php
+++ com_gridview/administrator/components/com_gridview/views/gridview/tmpl/form.php
@@ -1,1 +1,19 @@
-gridview
+<?= @helper('behavior.mootools') ?>
+<? defined('KOOWA') or die; ?>
+<script src="media://lib_koowa/js/koowa.js" />
+
+<form action="<?= @route('id='. $gridview->id) ?>" method="post" class="-koowa-form">
+	<div class="width-60 fltlft">
+		<fieldset class="adminform">
+			<legend><?= @text('General Settings') ?></legend>
+			
+			<ul class="adminformlist">
+				<li>
+					<label for="field_title"><?= @text('Title') ?>:</label>
+					<input type="text" id="field_title" name="title" value="<?= $gridview->title ?>" />
+				</li>
+			</ul>
+		</fieldset>
+	</div>
+</form>
+
