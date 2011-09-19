b24b69b1d1f01433dbb14ac27dad6d982519506a
Jonathan Bennett <jbennett@ccistudios.com>
2011-09-19 00:52:09 -0400

    Added grid view component and structure

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -7,4 +7,4 @@ role :app, "dev.ccistudios.com"
 # directories
 set :deploy_to, "/home/ccistud/subdomains/dev"
 set :public, "#{deploy_to}/public_html"
-set :extensions, %w[plg_ie6 public template]
+set :extensions, %w[com_gridview plg_ie6 public template]
