78e7f60a7339e8ac32c18f9b6340a55b3e37de60
Jonathan Bennett <jbennett@ccistudios.com>
2011-09-19 00:48:25 -0400

    Updated project configuration

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,14 +3,14 @@ set :stages, %w(staging production)
 set :default_stage, "staging"
 require "capistrano/ext/multistage"
 
-set :application, "APPLICATION_NAME"
+set :application, "CCI Studios Website"
 
 # repository info
-set :repository,  "git@github.com:CCI-Studios/PROJECT.git"
+set :repository,  "git@github.com:CCI-Studios/CCI-Studios.git"
 set :scm, :git
 
 # ssh settings
-set :user, "USERNAME"
+set :user, "ccistud"
 set :use_sudo, false
 
 # Joomla
